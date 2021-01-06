461f9598f7efb4b3c1895d4aabe80c6daa8bdc96
Kirsan <webmasters@list.ru>
2021-01-06 17:17:40 +0300

    [!] Update repository

--- Layouts/Layouts/Models/Repository.cs
+++ Layouts/Layouts/Models/Repository.cs
@@ -8,16 +8,9 @@ namespace Layouts.Models
 {
     public abstract class Repository
     {
-        //public int Id { get; set; }
-        //public string Brand { get; set; }
-        //public string Model { get; set; }
-        //public string System { get; set; }
-        //public int Price { get; set; }
-
         public const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Layouts;Integrated Security=True";
 
         public List<Repository> listItems = new List<Repository>();
         public abstract List<Repository> GetContent();
-
     }
 }
